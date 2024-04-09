e552a9e15f9212634de30043fb47379f9099b093
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-09 15:04:52 +0530

    Initial Folder Structure

---
+++ src/main/java/com/scatterrr/distributedfileserver/DistributedFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.scatterrr.distributedfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedFileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scatterrr/distributedfileserver/DistributedFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scatterrr.distributedfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedFileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
