21f9e338363a33e386d231c0dd0889f443543e49
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-14 01:09:38 +0530

    Updated the ValidationTest

--- src/test/java/com/scatterrr/distributedfileserver/ValidationTest.java
+++ src/test/java/com/scatterrr/distributedfileserver/ValidationTest.java
@@ -8,11 +8,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,24 +25,13 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ValidationTest {
 
-    private static final ArrayList<String> chunks = new ArrayList<>(){{
-        add("data-1.txt");
-        add("data-2.txt");
-        add("data-3.txt");
-        add("data-4.txt");
-        add("data-5.txt");
-        add("data-6.txt");
-        add("data-7.txt");
-        add("data-8.txt");
-        add("data-9.txt");
-        add("data-10.txt");
-        add("data-11.txt");
-    }};
+    private static final ArrayList<byte[]> chunks = new ArrayList<>();
 
-    private String originalRoot;
     private static final String fileName = "data";
+    private String originalRoot;
 
     @Autowired
     private FileManager fileManager;
@@ -55,18 +44,14 @@ public class ValidationTest {
         byte[] content = Files.readAllBytes(Paths.get(ROOT + fileName + ".txt"));
         MultipartFile inputFile = new MockMultipartFile(fileName + ".txt",
                 fileName + ".txt", "text/plain", content);
-        ArrayList<String> chunks = fileManager.chunkFile(inputFile);
+        chunks.addAll(fileManager.chunkFile(inputFile));
 
-        File outputDir = new File(ROOT + "output/");
-        if (!outputDir.exists()) {
-            outputDir.mkdirs();
-        }
         String merkleRoot = merkleTree.createMerkleTree(chunks);
 
-        FileOutputStream os = new FileOutputStream(ROOT + "output/merkleRoot.txt");
+        FileOutputStream os = new FileOutputStream(ROOT + "merkleRoot.txt");
         os.write(merkleRoot.getBytes());
         originalRoot = new String(
-                Files.readAllBytes(Paths.get(ROOT + "output/merkleRoot.txt"))
+                Files.readAllBytes(Paths.get(ROOT + "merkleRoot.txt"))
         );
     }
 
@@ -78,25 +63,17 @@ public class ValidationTest {
 
     @Test
     public void tamperedChunksTest() throws Exception{
-        ArrayList<String> tamperedChunks = new ArrayList<>(chunks);
-        String tampredChunk = tamperedChunks.get(0);
-        String originalcontent = new String(
-                Files.readAllBytes(Paths.get(ROOT + "chunks/" + fileName + "/" + tampredChunk)));
-
-        String tampredcontent = originalcontent + ">";
-
-        FileOutputStream os = new FileOutputStream(ROOT + "chunks/" + fileName + "/" + tampredChunk);
-        os.write(tampredcontent.getBytes());
+        ArrayList<byte[]> tamperedChunks = new ArrayList<>(chunks);
+        byte[] tamperedChunk = tamperedChunks.get(0);
+        tamperedChunk[tamperedChunk.length - 1] = (byte) (tamperedChunk[tamperedChunk.length - 1] + 1);
 
         String merkleRoot = merkleTree.createMerkleTree(tamperedChunks);
         assertNotEquals(originalRoot, merkleRoot);
-
-        os.write(originalcontent.getBytes());
     }
 
     @Test
-    public void missingChunksTest() throws Exception{
-        ArrayList<String> tamperedChunks = new ArrayList<>(chunks);
+    public void missingChunksTest() throws Exception {
+        ArrayList<byte[]> tamperedChunks = new ArrayList<>(chunks);
         tamperedChunks.remove(0);
 
         String merkleRoot = merkleTree.createMerkleTree(tamperedChunks);
@@ -104,20 +81,9 @@ public class ValidationTest {
     }
 
     @Test
-    public void tamperedOriginalRootHashTest() throws Exception{
-        String tamperedRoot = originalRoot + ">";
-        String merkleRoot = merkleTree.createMerkleTree(chunks);
-        assertNotEquals(tamperedRoot, merkleRoot);
-    }
-
-    @Test
-    public void extraChunksTest() throws Exception{
-        String originalRoot = new String(
-                Files.readAllBytes(Paths.get(ROOT + "output/merkleRoot.txt"))
-        );
-
-        ArrayList<String> tamperedChunks = new ArrayList<>(chunks);
-        tamperedChunks.add("data-1.txt");
+    public void extraChunksTest() throws Exception {
+        ArrayList<byte[]> tamperedChunks = new ArrayList<>(chunks);
+        tamperedChunks.add(new byte[0]);
 
         String merkleRoot = merkleTree.createMerkleTree(tamperedChunks);
         assertNotEquals(originalRoot, merkleRoot);
@@ -125,15 +91,7 @@ public class ValidationTest {
 
     @After
     public void tearDown() throws Exception{
-        Path path = Path.of(ROOT + "chunks/");
-        Files.walk(path)
-                .filter(Files::isRegularFile)
-                .map(Path::toFile)
-                .forEach(File::deleteOnExit);
-        Files.delete(Path.of(ROOT + "output/merkleRoot.txt"));
-        new File(ROOT + "chunks/"+ fileName).deleteOnExit();
-        new File(ROOT + "chunks/").deleteOnExit();
-        new File(ROOT + "output/").deleteOnExit();
+        Files.delete(Path.of(ROOT + "merkleRoot.txt"));
     }
 
 }
