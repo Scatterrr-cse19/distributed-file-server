b8cfd67970183315e97393e93f7a6acd72cd29f5
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-15 02:36:31 +0530

    Get the nodes list inside the MetadataService

--- src/main/java/com/scatterrr/distributedfileserver/service/MetadataService.java
+++ src/main/java/com/scatterrr/distributedfileserver/service/MetadataService.java
@@ -1,5 +1,7 @@
 package com.scatterrr.distributedfileserver.service;
 
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import com.scatterrr.distributedfileserver.dto.Node;
 import com.scatterrr.distributedfileserver.model.Metadata;
 import com.scatterrr.distributedfileserver.repository.FileServerRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class MetadataService {
     private final FileServerRepository fileServerRepository;
     private final FileManager fileManager;
     private final MerkleTree merkleTree;
+    private final PeerAwareInstanceRegistry registry;
 
     public void saveMetadata(MultipartFile file) throws Exception{
         ArrayList<byte[]> chunks = fileManager.chunkFile(file);
@@ -31,6 +35,15 @@ public class MetadataService {
 
     // TODO: Implement distribute chunks method
     private String distributeChunks(ArrayList<byte[]> chunkFileNames) {
+        ArrayList<Node> nodes = registry.getApplications().getRegisteredApplications().stream().
+                map(application ->
+                        application.getInstances().stream().map(instance ->
+                                new Node(
+                                        application.getName(),
+                                        instance.getHomePageUrl()
+                                )
+                        ).collect(Collectors.toCollection(ArrayList::new))
+                ).collect(ArrayList::new, ArrayList::addAll, ArrayList::addAll);
         return "<First Chunk URL>";
     }
 }
