3204f46f881d980b2a1edede3160e9e60b1a7327
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-07-25 21:08:22 +0530

    Fixed the chunk size

--- src/main/java/com/scatterrr/distributedfileserver/config/Config.java
+++ src/main/java/com/scatterrr/distributedfileserver/config/Config.java
@@ -1,12 +1,10 @@
 package com.scatterrr.distributedfileserver.config;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
-    public static final String ROOT = "src/main/resources/"; // TODO: TBD
-    public static final int CHUNK_SIZE = 15 * 1024; // TODO: Need to change
+    public static final String ROOT = "src/main/resources/";
+    public static final int CHUNK_SIZE = 64 * 1024 * 1024;
 
 }
